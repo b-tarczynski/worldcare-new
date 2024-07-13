17601dc803263261d2c95d8ed5766e6d9ec592be
Bartlomiej Tarczynski <51199571+b-tarczynski@users.noreply.github.com>
2024-07-13 10:36:18 +0200

    🍾 Add input and doctor info page (#12)
    Add input and doctor info page

---
+++ packages/nextjs/components/ui/Input.tsx
@@ -0,0 +1,14 @@
+interface InputProps {
+  label?: string
+  placeholder?: string
+  className?: string
+}
+
+export const Input = ({ label, placeholder, className }: InputProps) => {
+  return <label className={className}>
+    <div className="label">
+      {label && <span className="label-text">{label}</span>}
+    </div>
+    <input type="text" placeholder={placeholder} className="input input-bordered w-full bg-white" />
+  </label>
+}
