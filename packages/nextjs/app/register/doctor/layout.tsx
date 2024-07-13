ce91e8c433f5a20a6440d1d3c0186e3296710116
Bartlomiej Tarczynski <bartlomiej.tarczynski1@gmail.com>
2024-07-13 09:46:14 +0200

    Add image and text

--- packages/nextjs/app/register/doctor/layout.tsx
+++ packages/nextjs/app/register/doctor/layout.tsx
@@ -1,9 +1,17 @@
 import { Logo } from '~~/components/Logo'
+import { Heading1 } from "~~/components/ui/Heading1";
+import { ArrowRightIcon } from "@heroicons/react/24/solid";
 
 const RegisterLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="flex flex-row">
-      <div className="card h-screen w-[460px] bg-primary"></div>
+      <div className="card h-screen min-w-[460px] bg-primary flex flex-col items-center">
+        <div className="flex items-center gap-3 justify-center h-full">
+          <Heading1>I’m a doctor</Heading1>
+          <ArrowRightIcon className="h-8 w-8"/>
+        </div>
+        <img className="absolute bottom-0 left-0" src="/register-doctor.svg" alt=""/>
+      </div>
       <div className="w-full">
         <div className="flex flex-col justify-center h-24 w-full">
           <Logo/>
