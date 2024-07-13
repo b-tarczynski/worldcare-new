82920f10fb5a64660d1269816f18163cf06c1a7e
Wojstan <wpstaniszewski@gmail.com>
2024-07-13 12:08:17 +0200

    Add logout button

--- packages/nextjs/components/ui/Button.tsx
+++ packages/nextjs/components/ui/Button.tsx
@@ -2,8 +2,11 @@ import { ReactNode } from 'react'
 
 interface Props {
   children: ReactNode
+  className?: string
 }
 
-export function Button({ children }: Props) {
-  return <button className="btn btn-primary border-none rounded-full bg-primary min-w-60">{children}</button>
+export function Button({ children, className }: Props) {
+  return (
+    <button className={`btn btn-primary border-none rounded-full bg-primary min-w-60 ${className}`}>{children}</button>
+  )
 }
