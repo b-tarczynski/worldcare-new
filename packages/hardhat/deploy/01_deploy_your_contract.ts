397e8799425075df8639b7da42fa00858c187a11
Andrzej <andrzej.daniel@archblock.com>
2024-07-14 05:43:46 +0200

    Add arbitrum sepolia deployment

--- packages/hardhat/deploy/01_deploy_your_contract.ts
+++ packages/hardhat/deploy/01_deploy_your_contract.ts
@@ -6,6 +6,7 @@ const worldRouterAddresses = {
   sepolia: '0x469449f251692e0779667583026b5a1e99512157',
   optimismSepolia: '0x11cA3127182f7583EfC416a8771BD4d11Fae4334',
   baseSepolia: '0x42FF98C4E85212a5D31358ACbFe76a621b50fC02',
+  arbitrumSepolia: 'test',
 }
 
 /**
@@ -22,13 +23,33 @@ const deployYourContract: DeployFunction = async function (hre: HardhatRuntimeEn
     return
   }
 
-  if (hre.network.name != 'sepolia' && hre.network.name != 'optimismSepolia' && hre.network.name != 'baseSepolia') {
+  if (hre.network.name != 'sepolia' && hre.network.name != 'optimismSepolia' && hre.network.name != 'baseSepolia' && hre.network.name != 'arbitrumSepolia') {
     console.log('SEARCH THIS STAFF And add condition')
     return
   }
 
   const { deployer } = await hre.getNamedAccounts();
   const { deploy } = hre.deployments;
+
+
+  if (hre.network.name == 'arbitrumSepolia'){
+    const worldId =  await deploy("WorldId", {
+      from: deployer,
+      // Contract constructor arguments
+      log: true,
+      // autoMine: can be passed to the deploy function to make the deployment process faster on local networks by
+      // automatically mining the contract deployment transaction. There is no effect on live networks.
+      autoMine: true,
+    });
+  
+    if (!worldId.receipt?.contractAddress){
+      throw new Error('WorldId contract deployment failed')
+    }
+
+    worldRouterAddresses[hre.network.name] = worldId.receipt.contractAddress
+  }
+
+
 
   await deploy("WorldCare", {
     from: deployer,
