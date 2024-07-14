aa6762a8009b1b486fb9c044a34b377d95ca0014
Bartlomiej Tarczynski <bartlomiej.tarczynski1@gmail.com>
2024-07-14 04:35:50 +0200

    Use not hardcoded cids

--- packages/nextjs/app/history/share/shareHistory.ts
+++ packages/nextjs/app/history/share/shareHistory.ts
@@ -1,22 +1,16 @@
 'use server'
 
-import {shareFile} from "~~/utils/shareFile";
-import {privateBackendKey, publicBackendKey} from "../../../../../hardcodes";
-import {signAuthMessage} from "~~/utils/signAuthMessage";
+import { shareFile } from '~~/utils/shareFile'
+import { privateBackendKey, publicBackendKey } from '../../../../../hardcodes'
+import { signAuthMessage } from '~~/utils/signAuthMessage'
 
-// cids
-const patientVisitCids = [
-  'bafkreiececvwdsmzljjmwkr5zw74ruenjudeof4soes737tknytfcmvwbe',
-  'bafkreif52hzybepv44gsoqnigg7766pw4jbjwt4aa6kb76hch7nq2nahpm',
-]
-
-export async function shareHistory(doctorsAddress: string) {
+export async function shareHistory(doctorsAddress: string, cids: string[]) {
   console.log('doctor: ', doctorsAddress)
 
   const signedMessage = await signAuthMessage(privateBackendKey)
   console.log('signedMessage: ', signedMessage)
 
-  for (const cid of patientVisitCids) {
+  for (const cid of cids) {
     await shareFile(publicBackendKey, doctorsAddress, cid, signedMessage)
   }
 }
