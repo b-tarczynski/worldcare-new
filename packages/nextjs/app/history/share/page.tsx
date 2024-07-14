c8a630aed4e0b094633a63de6820b9c6c7db5af5
Antoni Koszowski <58612067+akoszowski@users.noreply.github.com>
2024-07-14 02:01:48 +0200

    💳  OnSubmit shareProfile (#48)
    * Update shareHistory
    
    * Add shareProfile onSubmit

--- packages/nextjs/app/history/share/page.tsx
+++ packages/nextjs/app/history/share/page.tsx
@@ -1,11 +1,43 @@
+'use client'
+
+import { useRouter } from 'next/navigation'
+import { useAccount } from 'wagmi'
+import { shareHistory } from '~~/app/history/share/shareHistory'
 import { BackButton } from '~~/components/ui/BackButton'
 import { Button } from '~~/components/ui/Button'
 import { Heading1 } from '~~/components/ui/Heading1'
 import { Heading3 } from '~~/components/ui/Heading3'
 import { Input } from '~~/components/ui/Input'
-import {shareHistory} from "~~/app/history/share/shareHistory";
+import { useScaffoldWriteContract } from '~~/hooks/scaffold-eth'
 
 export default function ShareHistory() {
+  const { isConnected, address: doctorsAddress } = useAccount()
+  if (!isConnected || !doctorsAddress) {
+    throw new Error('Not connected')
+  }
+
+  const { writeContractAsync: writeYourContractAsync } = useScaffoldWriteContract('WorldCare')
+
+  const router = useRouter()
+
+  const onSubmit = async (formData: FormData) => {
+    const doctorsAddress = formData.get('doctorsAddress') as string
+
+    await shareHistory(doctorsAddress)
+
+    await writeYourContractAsync(
+      {
+        functionName: 'shareProfile',
+        args: [doctorsAddress],
+      },
+      {
+        onSuccess: () => {
+          router.push('/history')
+        },
+      },
+    )
+  }
+
   return (
     <div>
       <BackButton href="/history" />
@@ -14,10 +46,10 @@ export default function ShareHistory() {
         <Heading1>Share data</Heading1>
         <Heading3 className="mt-4 mb-8">Search which doctor you want to share your medical data with</Heading3>
 
-        <form action={shareHistory}>
-          <Input id='doctorsAddress' placeholder='Elisa Doe' label='Doctor Address' />
+        <form action={onSubmit}>
+          <Input id="doctorsAddress" placeholder="Elisa Doe" label="Doctor Address" />
 
-          <Button className='mt-8'>Share data</Button>
+          <Button className="mt-8">Share data</Button>
         </form>
       </div>
     </div>
--- packages/nextjs/utils/shareFile.ts
+++ packages/nextjs/utils/shareFile.ts
@@ -2,5 +2,6 @@ import lighthouse from '@lighthouse-web3/sdk'
 
 export async function shareFile(ownerPK: string, recipientPK: string, cid: string, signedMessage: string) {
   const shareResponse = await lighthouse.shareFile(ownerPK, [recipientPK], cid, signedMessage)
+
   console.log('share response: ', shareResponse)
 }
