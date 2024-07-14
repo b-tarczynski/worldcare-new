97410f0793df786f32db89cba630bf44eb527067
akoszowski <antoni.koszowski@trusttoken.com>
2024-07-14 02:00:38 +0200

    Update shareHistory

--- packages/nextjs/app/history/share/shareHistory.ts
+++ packages/nextjs/app/history/share/shareHistory.ts
@@ -10,14 +10,13 @@ const patientVisitCids = [
   'bafkreif52hzybepv44gsoqnigg7766pw4jbjwt4aa6kb76hch7nq2nahpm',
 ]
 
-export async function shareHistory(formData: FormData) {
-  const doctorsAddress = formData.get('doctorsAddress')
+export async function shareHistory(doctorsAddress: string) {
   console.log('doctor: ', doctorsAddress)
 
   const signedMessage = await signAuthMessage(privateBackendKey)
   console.log('signedMessage: ', signedMessage)
 
   for (const cid of patientVisitCids) {
-    await shareFile(publicBackendKey, doctorsAddress as string, cid, signedMessage)
+    await shareFile(publicBackendKey, doctorsAddress, cid, signedMessage)
   }
 }
